cfa60fc0a3586617423ecded1b66a304fa13e743
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2022-04-19 20:28:44 +0200

    Lab5/6

---
+++ Lab5/main.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+
+#define SIZE 4
+
+using namespace std;
+
+int index[SIZE] = {0, 1, 2, 3};
+
+void show(double matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%f\t", matrix[index[i]][j]);
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void partial_pivoting(double matrix[SIZE][SIZE], int k) {
+    if (matrix[index[k]][k] == 0.0) {
+        int max_row = k + 1;
+        double max = fabs(matrix[index[k + 1]][k]);
+
+        for (int i = k + 1; i < SIZE; i++) {
+            double value = fabs(matrix[index[i]][k]);
+            if (value > max) {
+                max = value;
+                max_row = i;
+            }
+        }
+        swap(index[max_row], index[k]);
+    }
+}
+
+void gauss_elimination(double u[SIZE][SIZE], double l[SIZE][SIZE], int k) {
+    for (int i = k; i < SIZE - 1; i++) {
+        double coefficient = u[index[i + 1]][k] / u[index[k]][k];
+        for (int j = k; j < SIZE; j++) {
+            u[index[i + 1]][j] -= u[index[k]][j] * coefficient;
+        }
+        l[index[i + 1]][k] = coefficient;
+    }
+}
+
+void decomposition(double a[SIZE][SIZE], double l[SIZE][SIZE], double u[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            u[i][j] = a[i][j];
+        }
+    }
+    for (int k = 0; k < SIZE - 1; k++) {
+        partial_pivoting(u, k);
+        gauss_elimination(u, l, k);
+    }
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (i == j) {
+                l[index[i]][j] = 1.0;
+            } else if (i < j) {
+                l[index[i]][j] = 0.0;
+            }
+        }
+    }
+    cout << "Upper:" << endl;
+    show(u);
+    cout << "Lower:" << endl;
+    show(l);
+}
+
+void count_y(double l[SIZE][SIZE], double b[SIZE], double y[SIZE]) {
+    double sum;
+    for (int i = 0; i < SIZE; i++) {
+        sum = 0.0;
+        for (int j = 0; j < i; j++) {
+            sum += l[index[i]][j] * y[j];
+        }
+        y[i] = (b[index[i]] - sum) / l[index[i]][i];
+    }
+}
+
+void count_x(double u[SIZE][SIZE], double x[SIZE], double y[SIZE]) {
+    double sum;
+    for (int i = SIZE - 1; i >= 0; i--) {
+        sum = 0.0;
+        for (int j = i + 1; j < SIZE; j++) {
+            sum += u[index[i]][j] * x[j];
+        }
+        x[i] = (y[i] - sum) / u[index[i]][i];
+    }
+}
+
+bool check(double A[SIZE][SIZE], double b[SIZE], double x[SIZE]) {
+    int counter = 0;
+    double sum;
+    for (int i = 0; i < SIZE; i++) {
+        sum = 0.0;
+        for (int j = 0; j < SIZE; j++) {
+            sum += A[i][j] * x[j];
+        }
+        sum -= b[i];
+        if (sum == 0.0) {
+            counter++;
+        }
+    }
+    if (counter == SIZE) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+int main() {
+    double a[SIZE][SIZE] = {{1.0,   -20.0,  30.0,   -4.0},
+                            {2.0,   -40.0,  -6.0,   50.0},
+                            {9.0,   -180.0, 11.0,   -12.0},
+                            {-16.0, 15.0,   -140.0, 13.0}};
+    double b[SIZE] = {35.0, 104.0, -366.0, -354.0};
+    double l[SIZE][SIZE], u[SIZE][SIZE];
+    double x[SIZE], y[SIZE];
+    show(a);
+    decomposition(a, l, u);
+    count_y(l, b, y);
+    cout << "Ly = b" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        printf("y%d= %f\n", i + 1, y[i]);
+    }
+    count_x(u, x, y);
+    cout << "Ux = y" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        printf("x%d= %f\n", i + 1, x[i]);
+    }
+    if (check(a, b, x)) {
+        cout << "Wyniki sa poprawne" << endl;
+    }
+    return 0;
+}
---
+++ Lab6/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+
+#define SIZE 6
+using namespace std;
+
+auto c = new double[SIZE];
+
+void fill_u(double *u) {
+    u[0] = 1.0 / 2.0;
+    u[1] = 1.0 / 4.0;
+    u[2] = 1.0 / 6.0;
+    u[3] = 1.0 / 8.0;
+    u[4] = 1.0 / 10.0;
+}
+
+void fill_l(double *l) {
+    l[0] = 1.0 / 3.0;
+    l[1] = 1.0 / 5.0;
+    l[2] = 1.0 / 7.0;
+    l[3] = 1.0 / 9.0;
+    l[4] = 1.0 / 11.0;
+}
+
+void fill_d(double *d) {
+    d[0] = 10.0;
+    d[1] = 20.0;
+    d[2] = 30.0;
+    d[3] = 30.0;
+    d[4] = 20.0;
+    d[5] = 10.0;
+}
+
+void fill_b(double *b) {
+    b[0] = 31.0;
+    b[1] = 165.0 / 4.0;
+    b[2] = 917.0 / 30.0;
+    b[3] = 851.0 / 28.0;
+    b[4] = 3637.0 / 90.0;
+    b[5] = 332.0 / 11.0;
+}
+
+void count_eta(double *u, double *d, double *l) {
+    for (int i = 1; i < SIZE; i++) {
+        c[i] = l[i - 1] / d[i - 1];
+        d[i] -= (c[i] * u[i - 1]);
+    }
+}
+
+void count_r(double *b) {
+    for (int i = 1; i < SIZE; i++) {
+        b[i] -= c[i] * b[i - 1];
+    }
+}
+
+void results(double *d, double *u, double *r, double *x) {
+    for (int i = SIZE - 1; i >= 0; i--) {
+        if (i == (SIZE - 1)) {
+            x[i] = r[i] / d[i];
+        } else {
+            x[i] = (r[i] - (u[i] * x[i + 1])) / d[i];
+        }
+    }
+}
+
+void show(double *vector) {
+    for (int i = 0; i < SIZE; i++) {
+        printf("x%d = %f\n", i, vector[i]);
+    }
+    cout << endl;
+}
+
+bool check(double *u, double *d, double *l, double *x, double *b) {
+    int counter = 0;
+    double sum;
+    sum = d[0] * x[0] + u[0] * x[1];
+    if (b[0] - sum == 0.0) {
+        counter++;
+    }
+    for (int i = 0; i < SIZE - 1; i++) {
+        sum = l[i] * x[i] + d[i + 1] * x[i + 1] + u[i + 1] * x[i + 2];
+        if (b[i + 1] - sum == 0.0) {
+            counter++;
+        }
+    }
+    if (counter == SIZE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void thomas(double *u, double *d, double *l, double *b, double *x) {
+    count_eta(u, d, l);
+    count_r(b);
+    results(d, u, b, x);
+    show(x);
+}
+
+int main() {
+    auto u = new double[SIZE - 1];
+    auto d = new double[SIZE];
+    auto l = new double[SIZE - 1];
+    auto b = new double[SIZE];
+    auto x = new double[SIZE];
+    auto d2 = new double[SIZE];
+    auto b2 = new double[SIZE];
+    fill_u(u);
+    fill_l(l);
+    fill_d(d);
+    fill_b(b);
+    fill_d(d2);
+    fill_b(b2);
+
+    thomas(u, d, l, b, x);
+    if (check(u, d2, l, x, b2)) {
+        cout << "Good" << endl;
+    } else {
+        cout << "Bad" << endl;
+    }
+    return 0;
+}
+
